bdbbba843e1c2a28ee5000bf291356a67c3ea994
Dmitry <pepper16411@yandex.ru>
2016-11-20 19:51:00 +0400

    *	added straight LED on

--- light_controller.ino
+++ light_controller.ino
@@ -2,6 +2,8 @@ const byte ledPin = 10;
 const byte interrupt1Pin = 2;
 const byte interrupt2Pin = 3;
 volatile int counter = 0;
+byte lightArray[16]={0, 1, 2, 3, 5, 7, 10, 15, 22, 31, 44, 63, 90, 127, 180, 255};
+byte light = 0;
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(interrupt1Pin, INPUT_PULLUP);
@@ -14,20 +16,31 @@ void setup() {
 
 void loop() {  
   if (counter > 0) {
-    if (counter == 1) {
-      // выключаем
-      Serial.println("Led is off "); 
-      digitalWrite(ledPin, 0);
+    if ( counter == 180 ) {
+      light = 0;
+    }
+    if (counter > 164) {
+      // зажигаем 
+      analogWrite(ledPin, lightArray[light]);      
+      light++;
+    } 
+    if ( counter == 16 ) {
+      light = 15;
+    }
+    if (counter < 17) {
+      // гасим
+      analogWrite(ledPin, lightArray[light]);      
+      light--;
     }
     counter--;
-    delay(50);    
+    delay(200);    
     Serial.print("Conter is "); 
     Serial.println(counter, DEC);
   }
 }
 
 void change() {
-  counter = 7200;
+  counter = 180;
   Serial.print("Conter is "); 
   Serial.println(counter, DEC);
   digitalWrite(ledPin, 1);
